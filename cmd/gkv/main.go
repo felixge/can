134abe219e68435afb7cedf7f89df4558ba7be27
Felix Geisendörfer <felix@debuggable.com>
2014-12-15 14:30:13 +0100

    Basic HEAD support, WIP

--- cmd/gkv/main.go
+++ cmd/gkv/main.go
@@ -10,24 +10,41 @@ import (
 
 func main() {
 	log.SetFlags(0)
-	g := gkv.NewGKV(gkv.NewFileBackend("gkv"))
+	rp := gkv.NewRepository(gkv.NewFileBackend("gkv"))
 	blobs := make([]*gkv.Blob, 10)
 	entries := map[string]gkv.ID{}
 	for i := 0; i < len(blobs); i++ {
 		blob := gkv.NewBlob([]byte(fmt.Sprintf("val-%d", i)))
-		if err := g.Save(blob); err != nil {
+		if err := rp.Save(blob); err != nil {
 			log.Fatal(err)
 		}
 		blobs[i] = blob
 		entries[fmt.Sprintf("key-%d", i)] = blob.ID()
 	}
 	index := gkv.NewIndex(entries)
-	if err := g.Save(index); err != nil {
+	if err := rp.Save(index); err != nil {
 		log.Fatal(err)
 	}
 	commit := gkv.NewCommit(time.Now(), index.ID())
-	if err := g.Save(commit); err != nil {
+	if err := rp.Save(commit); err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("%s %s", commit.ID(), commit.Raw())
+	if err := rp.SetHead(commit.ID()); err != nil {
+		log.Fatal(err)
+	}
+	{
+		head, err := rp.Head()
+		if err != nil {
+			log.Fatal(err)
+		}
+		commit, err := rp.Commit(head)
+		if err != nil {
+			log.Fatal(err)
+		}
+		index, err := rp.Index(commit.Index())
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("%s", index.Raw())
+	}
 }
--- gkv.go
+++ gkv.go
@@ -3,6 +3,7 @@ package gkv
 import (
 	"bytes"
 	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -15,7 +16,16 @@ func NewID(o Object) ID {
 }
 
 func ParseId(id string) (ID, error) {
-	return ID{}, nil
+	r := ID{}
+	if len(id) != 40 {
+		return r, fmt.Errorf("bad id: %s: not 40 chars", id)
+	}
+	d, err := hex.DecodeString(id)
+	if err != nil {
+		return r, fmt.Errorf("bad id: %s: %s", id, err)
+	}
+	copy(r[:], d)
+	return r, nil
 }
 
 type ID [20]byte
@@ -29,21 +39,86 @@ type Object interface {
 	Raw() []byte
 }
 
-func NewGKV(b Backend) *GKV {
-	return &GKV{backend: b}
+func NewRepository(b Backend) *Repository {
+	return &Repository{backend: b}
 }
 
-type GKV struct {
+type Repository struct {
 	backend Backend
 	index   *Index
 }
 
-func (g *GKV) Save(o Object) error {
-	return g.backend.SaveObject(o.ID(), o.Raw())
+func (r *Repository) Head() (ID, error) {
+	return r.backend.Head()
 }
 
-func (g *GKV) Load(ref string) (Object, error) {
-	return nil, nil
+func (r *Repository) SetHead(id ID) error {
+	return r.backend.SetHead(id)
+}
+
+func (r *Repository) Commit(id ID) (*Commit, error) {
+	obj, err := r.Load(id)
+	if err != nil {
+		return nil, err
+	} else if commit, ok := obj.(*Commit); !ok {
+		return nil, fmt.Errorf("unexpected type: %T", obj)
+	} else {
+		return commit, nil
+	}
+}
+
+func (r *Repository) Index(id ID) (*Index, error) {
+	obj, err := r.Load(id)
+	if err != nil {
+		return nil, err
+	} else if index, ok := obj.(*Index); !ok {
+		return nil, fmt.Errorf("unexpected type: %T", obj)
+	} else {
+		return index, nil
+	}
+}
+
+func (r *Repository) Save(o Object) error {
+	return r.backend.SaveObject(o.ID(), o.Raw())
+}
+
+func (r *Repository) Load(id ID) (Object, error) {
+	raw, err := r.backend.GetObject(id)
+	if err != nil {
+		return nil, err
+	}
+	buf := bytes.NewBuffer(raw)
+	var (
+		kind string
+		size int64
+	)
+	if _, err := fmt.Fscanf(buf, "%s %d\n", &kind, &size); err != nil {
+		return nil, err
+	}
+	switch kind {
+	case "commit":
+		var (
+			sec    int64
+			offset int
+			index  string
+		)
+		// @TODO support negative offset
+		if _, err := fmt.Fscanf(buf, "time %d %d\n", &sec, &offset); err != nil {
+			return nil, err
+		}
+		t := time.Unix(sec, 0).In(time.FixedZone("", offset))
+		if _, err := fmt.Fscanf(buf, "index %s\n", &index); err != nil {
+			return nil, err
+		}
+		indexID, err := ParseId(index)
+		if err != nil {
+			return nil, fmt.Errorf("bad index: %s", err)
+		}
+		commit := &Commit{time: t, index: indexID}
+		return commit, nil
+	default:
+		return nil, fmt.Errorf("unknown object kind: %s", kind)
+	}
 }
 
 func NewIndex(entries map[string]ID) *Index {
@@ -67,18 +142,22 @@ func (idx *Index) Raw() []byte {
 	return append(header, buf.Bytes()...)
 }
 
-func NewCommit(time time.Time, partial ID, parents ...ID) *Commit {
-	return &Commit{time: time, partial: partial, parents: parents}
+func NewCommit(time time.Time, index ID, parents ...ID) *Commit {
+	return &Commit{time: time, index: index, parents: parents}
 }
 
 type Commit struct {
 	time    time.Time
-	partial ID
+	index   ID
 	parents []ID
 }
 
 func (c *Commit) ID() ID {
 	return NewID(c)
+}
+
+func (c *Commit) Index() ID {
+	return c.index
 }
 
 func (c *Commit) Raw() []byte {
@@ -89,7 +168,7 @@ func (c *Commit) Raw() []byte {
 		sign = "-"
 	}
 	fmt.Fprintf(buf, "time %d %s%d\n", c.time.Unix(), sign, offset)
-	fmt.Fprintf(buf, "partial %s\n", c.partial)
+	fmt.Fprintf(buf, "index %s\n", c.index)
 	for _, parent := range c.parents {
 		fmt.Fprintf(buf, "parent %s\n", parent)
 	}
@@ -120,6 +199,8 @@ func (b *Blob) Raw() []byte {
 type Backend interface {
 	GetObject(id ID) ([]byte, error)
 	SaveObject(id ID, raw []byte) error
+	Head() (ID, error)
+	SetHead(ID) error
 }
 
 func NewFileBackend(dir string) Backend {
@@ -131,27 +212,48 @@ type FileBackend struct {
 }
 
 func (f *FileBackend) GetObject(id ID) ([]byte, error) {
-	return ioutil.ReadFile(f.path(id))
+	return ioutil.ReadFile(f.objectPath(id))
 }
 
 func (f *FileBackend) SaveObject(id ID, raw []byte) error {
-	file, err := ioutil.TempFile("", id.String())
+	return f.writeAtomic(f.objectPath(id), raw)
+}
+
+func (f *FileBackend) Head() (ID, error) {
+	id := ID{}
+	data, err := ioutil.ReadFile(f.headPath())
+	if err != nil {
+		return id, err
+	}
+	copy(id[:], data)
+	return id, nil
+}
+
+func (f *FileBackend) SetHead(id ID) error {
+	return f.writeAtomic(f.headPath(), id[:])
+}
+
+func (f *FileBackend) writeAtomic(path string, data []byte) error {
+	file, err := ioutil.TempFile("", "")
 	if err != nil {
 		return err
 	}
 	defer file.Close()
-	p := f.path(id)
-	if _, err := file.Write(raw); err != nil {
+	if _, err := file.Write(data); err != nil {
 		return err
-	} else if err := os.MkdirAll(filepath.Dir(p), 0777); err != nil {
+	} else if err := os.MkdirAll(filepath.Dir(path), 0777); err != nil {
 		return err
-	} else if err := os.Rename(file.Name(), p); err != nil {
+	} else if err := os.Rename(file.Name(), path); err != nil {
 		return err
 	}
-	return ioutil.WriteFile(p, raw, 0666)
+	return nil
 }
 
-func (f *FileBackend) path(id ID) string {
+func (f *FileBackend) headPath() string {
+	return filepath.Join(f.dir, "HEAD")
+}
+
+func (f *FileBackend) objectPath(id ID) string {
 	s := id.String()
-	return filepath.Join(f.dir, s[0:2], s[2:])
+	return filepath.Join(f.dir, "objects", s[0:2], s[2:])
 }
