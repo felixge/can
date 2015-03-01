09099dc2fcfacdbfc4f47040b1842fb7cee799f9
Felix Geisendörfer <felix@debuggable.com>
2015-03-01 23:03:28 +0100

    Lots of sugar

--- sugar.go
+++ sugar.go
@@ -6,16 +6,89 @@ import (
 	"io"
 )
 
-func NewSugar(rp Repo) *Sugar {
-	return &Sugar{Repo: rp}
+func NewSugar(rp Repo) Sugar {
+	return &sugar{Repo: rp}
 }
 
-type Sugar struct {
+type Sugar interface {
+	Repo
+	HeadCommit() (Commit, error)
+	Keys(treeID ID, prefix []string) (KeyIterator, error)
+	Get(key []string) (io.ReadCloser, error)
+	Set(treeID ID, key []string, blob io.Reader) (ID, error)
+}
+
+type sugar struct {
 	Repo
 }
 
+// HeadCommit returns the head commit, or an error.
+func (s *sugar) HeadCommit() (Commit, error) {
+	if id, err := s.Head(); err != nil {
+		return Commit{}, err
+	} else {
+		return s.Commit(id)
+	}
+}
+
+func (s *sugar) Keys(treeID ID, prefix []string) (KeyIterator, error) {
+	var (
+		tree Tree
+		err  error
+	)
+	for _, name := range prefix {
+		if tree, err = s.Tree(treeID); err != nil {
+			return nil, err
+		} else if entry := tree.Get(name); entry == nil {
+			return nil, notFoundError(fmt.Sprintf("entry %q not found for prefix: %#v", name, prefix))
+		} else if entry.Kind != KindTree {
+			return nil, notFoundError(fmt.Sprintf("entry %q is %s for prefix: %#v", name, entry.Kind, prefix))
+		} else {
+			treeID = entry.ID
+		}
+	}
+	return &keyIterator{key: prefix, rp: s.Repo, stack: []Tree{tree}}, nil
+}
+
+type KeyIterator interface {
+	Next() ([]string, ID, error)
+}
+
+type keyIterator struct {
+	key   []string
+	rp    Repo
+	stack []Tree
+}
+
+func (k *keyIterator) Next() ([]string, ID, error) {
+	for {
+		if len(k.stack) == 0 {
+			return nil, nil, io.EOF
+		} else if tree := k.stack[len(k.stack)-1]; len(tree) == 0 {
+			k.stack = k.stack[:len(k.stack)-1]
+			if len(k.stack) == 0 {
+				continue
+			}
+			k.stack[len(k.stack)-1] = k.stack[len(k.stack)-1][1:]
+			k.key = k.key[:len(k.key)-1]
+		} else if entry := tree[0]; entry.Kind == KindTree {
+			if tree, err := k.rp.Tree(entry.ID); err != nil {
+				return nil, nil, err
+			} else {
+				k.stack = append(k.stack, tree)
+				k.key = append(k.key, entry.Name)
+			}
+		} else if entry.Kind == KindBlob {
+			k.stack[len(k.stack)-1] = tree[1:]
+			return append(k.key, entry.Name), entry.ID, nil
+		} else {
+			return nil, nil, fmt.Errorf("corrupt tree: %s", entry.ID)
+		}
+	}
+}
+
 // Get returns a read closer for the Blob with the given key.
-func (s *Sugar) Get(key []string) (io.ReadCloser, error) {
+func (s *sugar) Get(key []string) (io.ReadCloser, error) {
 	head, err := s.Head()
 	if err != nil {
 		return nil, err
@@ -46,20 +119,14 @@ func (s *Sugar) Get(key []string) (io.ReadCloser, error) {
 // a head prior to calling Set. Set may return neither ID nor error, which
 // means that no commit was created because the repo already had the desired
 // key value pair.
-func (s *Sugar) Set(key []string, blob io.Reader, commit *Commit) (ID, error) {
+func (s *sugar) Set(treeID ID, key []string, blob io.Reader) (ID, error) {
 	if len(key) == 0 {
 		return nil, errors.New("empty key")
 	}
 	// First we try to fetch the current head and all existing trees that we have
 	// need to merge with.
 	var trees []Tree
-	head, err := s.Head()
-	if err == nil {
-		parent, err := s.Commit(head)
-		if err != nil {
-			return nil, err
-		}
-		treeID := parent.Tree
+	if treeID != nil {
 		for _, k := range key {
 			tree, err := s.Tree(treeID)
 			if err != nil {
@@ -72,9 +139,6 @@ func (s *Sugar) Set(key []string, blob io.Reader, commit *Commit) (ID, error) {
 				treeID = entry.ID
 			}
 		}
-		commit.Parents = append(commit.Parents, head)
-	} else if err != nil && !IsNotFound(err) {
-		return nil, err
 	}
 	// Then we create the blob
 	blobID, err := s.WriteBlob(blob)
@@ -104,9 +168,9 @@ func (s *Sugar) Set(key []string, blob io.Reader, commit *Commit) (ID, error) {
 			// Add the entry to the tree and write it out
 			if prevTreeID, err = s.WriteTree(tree.Add(entry)); err != nil {
 				return nil, err
-				// If this is the root tree, set it on the given commit.
+				// If this is the root tree, we are done
 			} else if i == 0 {
-				commit.Tree = prevTreeID
+				break
 			}
 			// If this is the first tree node (the leaf node) and there was no need
 			// for an update, we don't need to commit anything as the tree remains
@@ -119,10 +183,5 @@ func (s *Sugar) Set(key []string, blob io.Reader, commit *Commit) (ID, error) {
 			return nil, fmt.Errorf("corrupt tree: key=%#v tree=%#v", key, tree)
 		}
 	}
-	// Finally we create the commit with the new Tree and update our head.
-	if commitID, err := s.WriteCommit(*commit); err != nil {
-		return nil, err
-	} else {
-		return commitID, s.WriteHead(commitID)
-	}
+	return prevTreeID, nil
 }
